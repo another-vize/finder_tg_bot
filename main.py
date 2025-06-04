6acb92440cd7eeff9352ac074b90b0c15be46f98
Vize <qwervize124@gmail.com>
2025-06-04 09:08:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from app.handlers import router
 from aiogram import Bot, Dispatcher
 
 async def main():
-    bot = Bot(token='7862382488:AAGR3hEZO8YFl-95NiV7pxLvQyO2_1J5qR8')
+    bot = Bot(token='______')
     dp = Dispatcher()
     dp.include_router(router)
     await dp.start_polling(bot)
